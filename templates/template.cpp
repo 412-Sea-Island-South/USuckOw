f42485b70f3a61b68cb7701c774c4b163f75be86
Black Angel <81530826+B-L-4-C-K-4-N-G-E-L@users.noreply.github.com>
2022-03-24 19:00:36 -0700

    Create template.cpp

---
+++ templates/template.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long double ld;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+
+//_________________________________________
+/* GLOBAL VARIABLES
+//int and ll variables here
+
+-------------------------------------------
+//vectors and static arrays here
+
+-------------------------------------------
+//other data structures here
+
+*/
+//_________________________________________
+
+void fast(void){
+	ios_base::sync_with_stdio(0);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return;
+}
+
+//_________________________________________
+/* UTIILITY FUNCTIONS
+// return_type dfs(function args){}
+// return_type bfs(function args){}
+// return_type solve(function args){}
+
+*/
+//_________________________________________
+
+int main(void){
+	fast();
+	freopen("problemname.in", "r", stdin);
+	freopen("problemname.out", "w", stdout);
+	cout << "Hello World" << endl;
+	return 0;
+}
+
+/*
+PROB: problemname
+LANG: C++17
+*/
